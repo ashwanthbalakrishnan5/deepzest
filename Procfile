326764f905e32c576f83d89c21ae1ffdf025fee6
Ashwanth <ashwanthbalakrishnan5@gmail.com>
2023-09-07 19:19:57 +0530

    added database url link

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tfoi.wsgi:application
--- deepzest/settings.py
+++ deepzest/settings.py
@@ -76,12 +76,11 @@ WSGI_APPLICATION = "deepzest.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
+import os
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
 
 
 # Password validation
